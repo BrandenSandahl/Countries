c4c83fde854caa95f96a0c3c604c9b9239821a99
BrandenSandahl <branden157@gmail.com>
2016-02-15 09:27:28 -0500

    Changed the char test method. (removed the .char call

--- src/Countries.java
+++ src/Countries.java
@@ -24,7 +24,7 @@ public class Countries {
             System.out.println("Please choose the starting letter of the states you wish to display:");
             System.out.println("IE, \"A\" or \"B\"");
             userChoice = scanner.nextLine().toUpperCase().charAt(0);
-            if (userChoice.charValue() >= 65 && userChoice.charValue() <=90) //this is testing for A-Z
+            if (userChoice >= 65 && userChoice <=90) //this is testing for A-Z
                 break;
         }
 
@@ -35,6 +35,7 @@ public class Countries {
         for (Country c : tempOutput) {
             output += c.getCountryShort() + " | " + c.getCountryName() + "\n";
         }
+
 
         String fileName = userChoice + "_Countries.txt";
         String fileNameJson = userChoice + "_Countries.json";
@@ -49,17 +50,18 @@ public class Countries {
     public static void readFileToHashMap() throws FileNotFoundException {
         File f = new File("countries.txt");
         Scanner fileScanner = new Scanner(f);  //try catch here?
+
         while (fileScanner.hasNext()) {  //going through the file line by line
             String[] lineIn = fileScanner.nextLine().split("\\|");         //turning a line into string array on the |
             Country tempCountry = new Country(lineIn[0], lineIn[1]);           //creating a temp object and loading it from the string
-            Character firstLetter = lineIn[1].toUpperCase().charAt(0);         //making my key
+            Character key = lineIn[1].toUpperCase().charAt(0);         //making my key
 
-            if (!countryMap.keySet().contains(firstLetter)) { //if the key has not yet been created
-                countryMap.put(firstLetter, new ArrayList<>());  //create key and empty array. //CAN I COMBINE THESE LINES?
-                countryMap.get(firstLetter).add(tempCountry); //go ahead and add in the country. I mean..why not?! Why not bro. Why not
-            } else {
-                countryMap.get(firstLetter).add(tempCountry); //otherwise just pull up the key and add the object value to the array
+            if (!countryMap.keySet().contains(key)) { //if the key has not yet been created
+                countryMap.put(key, new ArrayList<>());
             }
+
+            countryMap.get(key).add(tempCountry); //otherwise just pull up the key and add the object value to the array
+
 
         }
     }
